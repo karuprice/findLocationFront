4f08fa23b997ca4b24ce97f8349d620ce57d31ac
Karl Price <karuprice@gmail.com>
2017-10-12 16:59:03 +0100

    end of play

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,8 +10,17 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 
-    .controller('View1Ctrl', ['$scope', function($scope) {
-        $scope.msg = "I'm from controller View1Ctrl's $scope"
+    .controller('View1Ctrl', ['$scope','$http',function($scope, $http) {
+        $scope.msg = "I'm from controller View1Ctrl's $scope";
+
+        $http({
+            method: 'GET',
+            url: 'http://127.0.0.1:8001/api'
+        }).then(function success(res) {console.log(res);
+            $scope.apiData = res.data;
+        }, function error(res) {
+            $scope.apiData = res.statusText;
+        });
     }]);
 
 
